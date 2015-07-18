c50a368233942da6d964edc958e1df785a2f1092
MV <vmanikandan001@hotmail.com>
2015-07-18 19:10:44 +0100

    V1 - Ship ready

--- www/scripts/controllers.js
+++ www/scripts/controllers.js
@@ -1,12 +1,11 @@
 angular.module('app.controllers', [])
-.controller('radioController', function ($scope, $ionicLoading, radioDataService) {
+.controller('radioController', function ($scope, $ionicLoading, $ionicPopup, $timeout, radioDataService) {
     var media;
     var vm = this;
-    vm.isPaused = false;
+    vm.isStopped = false;
     vm.currentRadio = null;
     vm.volume = 50;
-
-
+    var mediaStartingPromise = null;
 
     activate();
 
@@ -17,20 +16,50 @@ angular.module('app.controllers', [])
     });
 
     vm.play = function (radio) {
-        showLoading();
         vm.currentRadio = radio;
-        vm.isPaused = false;
+        vm.isStopped = false;
 
         if (media) {
             media.release();
         }
 
-        media = new Media(radio.streamUrl);
+        media = new Media(radio.streamUrl, null, mediaError, mediaStatus);
+
         media.play();
-        hideLoading();
     }
 
-    vm.pauseToggle = function () {
+    function mediaError(error) {
+        showAlert("Error", "Unable to play this radio. Please try again later.");
+
+        //MediaError.MEDIA_ERR_ABORTED = 1
+        //MediaError.MEDIA_ERR_NETWORK = 2
+        //MediaError.MEDIA_ERR_DECODE = 3
+        //MediaError.MEDIA_ERR_NONE_SUPPORTED = 4
+    }
+
+    function mediaStatus(status) {
+        if (status === Media.MEDIA_STARTING) {
+            showLoading("Starting " + vm.currentRadio.name + "...");
+            mediaStartingPromise = $timeout(onStrukStarting, 15000); // wait 15 seconds
+        }
+
+        if (status === Media.MEDIA_RUNNING) {
+            $timeout.cancel(mediaStartingPromise);
+            hideLoading(); // hide the status started when media is starting
+        }
+
+        function onStrukStarting() {
+            hideLoading();
+            showAlert("Error", "Unable to play this radio. Please try again later.");
+        }
+        //Media.MEDIA_NONE = 0;
+        //Media.MEDIA_STARTING = 1;
+        //Media.MEDIA_RUNNING = 2;
+        //Media.MEDIA_PAUSED = 3;
+        //Media.MEDIA_STOPPED = 4;
+    }
+
+    vm.stopToggle = function () {
 
         // when no radio is selected and play is pressed then select the first one in the list and play
         if (!vm.currentRadio) {
@@ -40,13 +69,13 @@ angular.module('app.controllers', [])
 
         showLoading();
         if (media) {
-            if (vm.isPaused) {
-                vm.isPaused = false;
+            if (vm.isStopped) {
+                vm.isStopped = false;
                 media.play();
 
             } else {
-                vm.isPaused = true;
-                media.pause();
+                vm.isStopped = true;
+                media.stop();
             }
         }
 
@@ -63,9 +92,9 @@ angular.module('app.controllers', [])
         document.addEventListener("deviceready", onDeviceReady, false);
     }
 
-    function showLoading() {
+    function showLoading(msg) {
         $ionicLoading.show({
-            template: 'Please wait...'
+            template: msg || 'Please wait...'
         });
     }
 
@@ -84,8 +113,38 @@ angular.module('app.controllers', [])
         .then(function (response) {
             vm.radios = response.data.radios;
         })
+        .catch(function () {
+            showInternetIssueAlert("Error", "Unable to retrieve radio list. Please make sure you have internet access. If the problem persist, try again later.", getRadios);
+        })
         .finally(function () {
             hideLoading();
+        });
+    }
+
+    function showAlert(title, message) {
+        var alertPopup = $ionicPopup.alert({
+            title: title,
+            template: message
+        });
+    }
+
+    function showInternetIssueAlert(title, message, onRetry) {
+        $ionicPopup.show({
+            template: message,
+            title: title,
+            buttons: [
+              {
+                  text: "Re-try",
+                  type: "button-assertive",
+                  onTap: function (e) {
+                      onRetry();
+                  }
+              },
+              {
+                  text: "Close",
+                  type: "button-positive"
+              }
+            ]
         });
     }
 })
